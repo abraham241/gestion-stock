cb8b3416e913775f151be8fe343082c0cf19e488
cruz-welandi <cruzwelandi@gmail.com>
2024-10-04 22:12:43 +0100

    ajout de la nav bar dans le layout du dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { onAuthStateChanged } from "firebase/auth";
+import { Auth } from "@/Firebase/firebase.config";
+import NavBar from '@/components/navBar';
+import SideBar from '@/components/sideBar';
 
-const layout = () => {
+
+
+
+const layout = ({children}: Readonly<{children: React.ReactNode}>) => {
+
+  const verifedSingIn = () => {
+
+    const auth = Auth;
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+        // ...
+      } else {
+        // User is signed out
+        // ...
+      }
+    });
+
+  }
+
   return (
-    <div>layout</div>
+    <main className='h-[100vh] w-full'>
+      <div className='h-[10%] w-full bg-emerald-400'>
+        <NavBar className='h-full'/>
+      </div>
+      <div className='h-[90%] w-full flex'>
+        <div className='w-36 bg-red-500'>
+          <SideBar/>
+        </div>
+        <div className='flex-1'>
+          {children}
+        </div>
+      </div>
+    </main>
   )
 }
 
--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,31 +1,35 @@
 import React from "react";
 import { AiFillBell } from "react-icons/ai";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 // import profil from "../public/images/profil.jpg";
 
-const NavBar = () => {
+type navBarProps ={
+  className?: string
+}
+
+const NavBar: React.FC<navBarProps> = ({className}) => {
   return (
-    <div>
-      <div className="navbar bg-base-100 flex justify-around items-center h-20 w-[1000px] bg-slate-500">
-        <div className="">
-          <a className="btn btn-ghost text-3xl font-bold ">Dashboard</a>
+    <div className={cn(className, 'bg-base-100 flex justify-around items-center')}>
+      <div className="">
+        <a className="btn-ghost text-3xl font-bold ">Dashboard</a>
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Recherche"
+          className="input  w-96 h-10 border-gray-400 rounded border-2 outline-none"
+        />
+      </div>
+      <div className="flex gap-5">
+        <div className="text-2xl">
+          <AiFillBell />
         </div>
-        <div>
-          <input
-            type="text"
-            placeholder="Recherche"
-            className="input  w-96 h-10 border-gray-400 rounded border-2 outline-none"
-          />
-        </div>
-        <div className="flex gap-5">
-          <div className="text-2xl">
-            <AiFillBell />
-          </div>
-          <div className="h-12 w-14 bg-black rounded-full">
-            {/* <Image src={profil} alt="" height={100} width={100} /> */}
-          </div>
+        <div className="h-12 w-14 bg-black rounded-full">
+          {/* <Image src={profil} alt="" height={100} width={100} /> */}
         </div>
       </div>
+
     </div>
   );
 };
